ca3d9809ea19bd5a3728a065d1a797ef1a06a616
YourGitHubUsername <YourEmail@example.com>
2025-07-08 16:43:56 +0700

    fix login

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const path = require('path');
+const cors = require('cors'); // <- thêm dòng này
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'database.json')); // hoặc db.json nếu bạn đổi tên
@@ -7,6 +8,7 @@ const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000;
 
+server.use(cors()); // ✅ thêm dòng này để bật CORS
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
